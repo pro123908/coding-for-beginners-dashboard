97d8f6fa83e109d125f6141c48f77d9ab4537ede
Bilal Ahmad <pro123908@gmail.com>
2020-09-23 03:49:51 +0500

    Google Button with OrLine in login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ButtonWithIcon from "../../components/Misc/ButtonWithIcon/ButtonWithIcon";
+import GoogleButton from "../../components/Misc/GoogleButton/GoogleButton";
 import InputWithIcon from "../../components/Misc/InputWithIcon/InputWithIcon";
+import OrLine from "../../components/Misc/OrLine/OrLine";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -23,6 +25,10 @@ const Login = () => {
           inputType="password"
         />
         <ButtonWithIcon buttonText="Continue" iconClass="fas fa-arrow-right" />
+
+        <OrLine />
+
+        <GoogleButton text="Sign in with Google" />
       </div>
     </div>
   );
