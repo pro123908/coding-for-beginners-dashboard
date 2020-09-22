fba90c7601fc10a16fc1c78ad1aba27776e25c87
Bilal Ahmad <pro123908@gmail.com>
2020-09-23 03:04:03 +0500

    State & Login responsive

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonWithIcon from "../../components/Misc/ButtonWithIcon/ButtonWithIcon";
 import InputWithIcon from "../../components/Misc/InputWithIcon/InputWithIcon";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="cfb-login">
       <div className="cfb-login__box">
         <div className="cfb-login__heading">Login to continue</div>
-        <InputWithIcon placeholder="Email Address" />
-        <InputWithIcon placeholder="Password" iconClass="fas fa-lock" />
+        <InputWithIcon
+          placeholder="Email Address"
+          value={email}
+          onInputChange={setEmail}
+        />
+        <InputWithIcon
+          placeholder="Password"
+          iconClass="fas fa-lock"
+          value={password}
+          onInputChange={setPassword}
+          inputType="password"
+        />
         <ButtonWithIcon buttonText="Continue" iconClass="fas fa-arrow-right" />
       </div>
     </div>
