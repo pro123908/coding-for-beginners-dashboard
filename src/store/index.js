d17554cce33900041501a9ace270d7410230b49f
Bilal Ahmad <pro123908@gmail.com>
2020-09-28 21:31:00 +0500

    Comments added

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer";
 
+// Creating the store with the root reducer and async middleware redux thunk and exporting it
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
